79993024901ef4aaf769d91373949349d3257338
Robert Greiner <robert@robertgreiner.com>
2011-11-02 18:09:59 -0500

    Added class that sums the squares of the difference of similar reviews between two users.

---
+++ Recommender.Tests/Steps/SumDifferenceSquaresTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Recommender.Steps;
+
+namespace Recommender.Tests.Steps
+{
+    [TestFixture]
+    class SumDifferenceSquaresTests
+    {
+        [Test]
+        public void ShouldReturnZeroOnEmpty()
+        {
+            var emptyReviewer = new Dictionary<string, double>();
+            var similarReviews = new List<string>();
+            var sumDifferenceSquares = new SumDifferenceSquares(similarReviews, emptyReviewer, emptyReviewer).Calculate();
+            Assert.AreEqual(0.0, sumDifferenceSquares);
+        }
+
+        [Test]
+        public void ShouldReturnZeroWithIdenticalReviews()
+        {
+            var highScoreReviewer = new Dictionary<string, double>();
+            var similarReviews = new List<string>();
+            highScoreReviewer.Add("C# in Depth", 5);
+            similarReviews.Add("C# in Depth");
+            var sumDifferenceSquares = new SumDifferenceSquares(similarReviews, highScoreReviewer, highScoreReviewer).Calculate();
+            Assert.AreEqual(0.0, sumDifferenceSquares);
+        }
+
+        [Test]
+        public void ShouldCalculateTheSumOfTheSquareOfTheDifferenceOfASingleSimilarReview()
+        {
+            var highScoreReviewer = new Dictionary<string, double>();
+            var lowScoreReviewer = new Dictionary<string, double>();
+            var similarReviews = new List<string>();
+            highScoreReviewer.Add("C# in Depth", 5);
+            lowScoreReviewer.Add("C# in Depth", 3);
+            similarReviews.Add("C# in Depth");
+            var sumDifferenceSquares = new SumDifferenceSquares(similarReviews, highScoreReviewer, lowScoreReviewer).Calculate();
+            Assert.AreEqual(4.0, sumDifferenceSquares);
+        }
+
+    }
+}
---
+++ Recommender/Steps/SumDifferenceSquares.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recommender.Steps
+{
+    public class SumDifferenceSquares : SimilarityScoreCommand
+    {
+        private readonly List<string> SimilarReviews;
+        private readonly Dictionary<string, double> FirstUserReviews;
+        private readonly Dictionary<string, double> SecondUserReviews;
+
+        public SumDifferenceSquares(List<string> similarReviews, Dictionary<string, double> firstUserReviews, Dictionary<string, double> secondUserReviews)
+        {
+            SimilarReviews = similarReviews;
+            FirstUserReviews = firstUserReviews;
+            SecondUserReviews = secondUserReviews;
+        }
+
+        public double Calculate()
+        {
+            return SimilarReviews.Sum(title => Math.Pow(FirstUserReviews[title] - SecondUserReviews[title], 2));
+        }
+    }
+}
