bc64d2de5a778d11f736077e1d19447a92c9ad16
Robert Greiner <robert@robertgreiner.com>
2011-11-07 16:51:05 -0600

    added more scenarios to the similarity score runner.

--- Recommender/SimilarityScoreRunner.cs
+++ Recommender/SimilarityScoreRunner.cs
@@ -8,7 +8,10 @@ namespace Recommender
         static void Main(string[] args)
         {
             RunEuclideanDistanceWithLowScore();
+            RunPearsonCorrelationWithLowScore();
+            Console.WriteLine();
             RunEuclideanDistanceWithHighScore();
+            RunPearsonCorrelationWithHighScore();
             Console.ReadKey();
         }
 
@@ -18,16 +21,34 @@ namespace Recommender
             var r2 = ReviewerBuilder.BuildReviewer2();
             var euclideanDistance = new EuclideanDistance(r1, r2);
 
-            Console.WriteLine("The similarity score between {0} and {1} is: {2}", r1.Name, r2.Name, euclideanDistance.Score());
+            Console.WriteLine("The Euclidean Distance between {0} and {1} is: {2}", r1.Name, r2.Name, euclideanDistance.Score());
         }
 
         private static void RunEuclideanDistanceWithHighScore()
         {
-            var r1 = ReviewerBuilder.BuildReviewer3();
-            var r2 = ReviewerBuilder.BuildReviewer5();
+            var r1 = ReviewerBuilder.BuildReviewer4();
+            var r2 = ReviewerBuilder.BuildReviewer1();
             var euclideanDistance = new EuclideanDistance(r1, r2);
 
-            Console.WriteLine("The similarity score between {0} and {1} is: {2}", r1.Name, r2.Name, euclideanDistance.Score());
+            Console.WriteLine("The Euclidean Distance between {0} and {1} is: {2}", r1.Name, r2.Name, euclideanDistance.Score());
+        }
+
+        private static void RunPearsonCorrelationWithLowScore()
+        {
+            var r1 = ReviewerBuilder.BuildReviewer1();
+            var r2 = ReviewerBuilder.BuildReviewer2();
+            var pearsonCorrelation = new PearsonCorrelation(r1, r2);
+
+            Console.WriteLine("The Pearson Correlation between {0} and {1} is: {2}", r1.Name, r2.Name, pearsonCorrelation.Score());
+        }
+
+        private static void RunPearsonCorrelationWithHighScore()
+        {
+            var r1 = ReviewerBuilder.BuildReviewer4();
+            var r2 = ReviewerBuilder.BuildReviewer1();
+            var pearsonCorrelation = new PearsonCorrelation(r1, r2);
+
+            Console.WriteLine("The Pearson Correlation between {0} and {1} is: {2}", r1.Name, r2.Name, pearsonCorrelation.Score());
         }
     }
 }
