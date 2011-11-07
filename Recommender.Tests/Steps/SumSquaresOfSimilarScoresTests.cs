101da36be21b606e0b5c1e780a27ab445d4afef2
Robert Greiner <robert@robertgreiner.com>
2011-11-07 15:00:41 -0600

    added code to sum the squares of the scores of similar reviews for a single reviewer.

---
+++ Recommender.Tests/Steps/SumSquaresOfSimilarScoresTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Recommender.Common;
+using Recommender.Data;
+using Recommender.Reviews;
+using Recommender.Steps;
+
+namespace Recommender.Tests.Steps
+{
+    [TestFixture]
+    class SumSquaresOfSimilarScoresTests
+    {
+
+        private Reviewer r1;
+        private Reviewer r2;
+        private List<String> similarReviews;
+
+        [SetUp]
+        public void SetUp()
+        {
+            r1 = ReviewerBuilder.BuildReviewer1();
+            r2 = ReviewerBuilder.BuildReviewer2();
+            similarReviews = new FindSimilarReviews(r1.Reviews, r2.Reviews).Calculate();
+        }
+
+        [Test]
+        public void ShouldSumTheSquaresOfAllOfTheScoresFromTheFirstUser()
+        {
+            Assert.AreEqual(55.0, new SumSquaresOfSimilarScores(similarReviews, r1.Reviews).Calculate());
+        }
+
+        [Test]
+        public void ShouldSumTheSquaresOfAllOfTheScoresFromTheSecondUser()
+        {
+            Assert.AreEqual(69.75, new SumSquaresOfSimilarScores(similarReviews, r2.Reviews).Calculate());
+        }
+
+    }
+}
---
+++ Recommender/Steps/SumSquaresOfSimilarScores.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recommender.Steps
+{
+    public class SumSquaresOfSimilarScores : SimilarityScoreCommand
+    {
+        private readonly List<string> SimilarReviews;
+        private readonly Dictionary<string, double> UserReviews;
+
+        public SumSquaresOfSimilarScores(List<string> similarReviews, Dictionary<string, double> userReviews)
+        {
+            SimilarReviews = similarReviews;
+            UserReviews = userReviews;
+        }
+
+        public double Calculate()
+        {
+            return SimilarReviews.Sum(title => Math.Pow(UserReviews[title], 2));
+        }
+    }
+}
