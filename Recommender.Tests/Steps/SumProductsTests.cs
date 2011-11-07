195f3aad7ebb3ddb76c6a8b5ef695cf0657e05d2
Robert Greiner <robert@robertgreiner.com>
2011-11-07 15:08:41 -0600

    Added code to sum the products of each similar review between two users.

---
+++ Recommender.Tests/Steps/SumProductsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Recommender.Common;
+using Recommender.Data;
+using Recommender.Reviews;
+using Recommender.Steps;
+
+namespace Recommender.Tests.Steps
+{
+    [TestFixture]
+    class SumProductsTests
+    {
+        private Reviewer r1;
+        private Reviewer r2;
+        private List<String> similarReviews;
+
+        [SetUp]
+        public void SetUp()
+        {
+            r1 = ReviewerBuilder.BuildReviewer1();
+            r2 = ReviewerBuilder.BuildReviewer2();
+            similarReviews = new FindSimilarReviews(r1.Reviews, r2.Reviews).Calculate();
+        }
+
+        [Test]
+        public void ShouldSumTheProductsOfBothReviewers()
+        {
+            Assert.AreEqual(59.5, new SumProducts(similarReviews, r1.Reviews, r2.Reviews).Calculate());
+        }
+    }
+}
---
+++ Recommender/Steps/SumProducts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recommender.Steps
+{
+    public class SumProducts : SimilarityScoreCommand
+    {
+        private readonly List<string> SimilarReviews;
+        private readonly Dictionary<string, double> FirstUserReviews;
+        private readonly Dictionary<string, double> SecondUserReviews;
+
+
+        public SumProducts(List<string> similarReviews, Dictionary<string, double> firstUserReviews, Dictionary<string, double> secondUserReviews)
+        {
+            SimilarReviews = similarReviews;
+            FirstUserReviews = firstUserReviews;
+            SecondUserReviews = secondUserReviews;
+        }
+
+        public double Calculate()
+        {
+            return SimilarReviews.Sum(title => FirstUserReviews[title] * SecondUserReviews[title]);
+        }
+    }
+}
